b71fc9b424f8ae8602b601520d81fe50239f9222
elvissomers <elvissomers98@gmail.com>
2023-04-17 14:40:19 +0200

    Update DayOne.java
    Setup elf calorie array

--- src/main/java/com/project/controllers/DayOne.java
+++ src/main/java/com/project/controllers/DayOne.java
@@ -4,11 +4,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("1")
 public class DayOne {
     @GetMapping("1")
     public int getAnswerOne(){
+        String inputFilePath = "C:\\Users\\elvis\\projects\\aoc_new\\data\\scraped_data_1.txt";
+        ArrayList<Integer> elfCalories = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            String line;
+            int elfCount = 0;
+            while ((line = reader.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    Integer currentCalories = elfCalories.get(elfCount);
+                    elfCalories.set(elfCount, currentCalories + Integer.parseInt(line.trim()));
+                } else {
+                    elfCount += 1;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return 0;
     }
 }
