314cc5262172a67b9960524e67cb6899e2b3a0a8
elvissomers <elvissomers98@gmail.com>
2023-04-17 15:33:17 +0200

    Create DayTwo.java

---
+++ src/main/java/com/project/aoc/controllers/DayTwo.java
@@ -0,0 +1,63 @@
+package com.project.aoc.controllers;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("2")
+public class DayTwo {
+
+    @GetMapping("1")
+    public int getAnswerTwo(){
+        String inputFilePath = "C:\\Users\\elvis\\projects\\aoc_new\\data\\scraped_data_2.txt";
+        ArrayList<Integer> elfCalories = new ArrayList<>();
+        int score = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()){
+                    continue;// go to next line
+                }
+                char firstChar = line.charAt(0);
+                char secondChar = line.charAt(2);
+
+                if (secondChar == 'X'){
+                    score += 1;
+                    if (firstChar == 'A'){
+                        score += 3;
+                    } else if (firstChar == 'C'){
+                        score += 6;
+                    }
+                } else if (secondChar == 'Y'){
+                    score += 2;
+                    if (firstChar == 'B'){
+                        score += 3;
+                    } else if (firstChar == 'A'){
+                        score += 6;
+                    }
+                } else if (secondChar == 'Z'){
+                    score += 3;
+                    if (firstChar == 'C'){
+                        score += 3;
+                    } else if (firstChar == 'B'){
+                        score += 6;
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return score;
+    }
+
