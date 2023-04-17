b889db93c97dd0384c3f4e04f1b76ecef01ae82a
elvissomers <elvissomers98@gmail.com>
2023-04-17 15:56:58 +0200

    Update DayThree.java

--- src/main/java/com/project/aoc/controllers/DayThree.java
+++ src/main/java/com/project/aoc/controllers/DayThree.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,14 +18,25 @@ public class DayThree {
     @GetMapping("1")
     public int getAnswerThree(){
         String inputFilePath = "C:\\Users\\elvis\\projects\\aoc_new\\data\\scraped_data_2.txt";
+        List<Character> doubledItems = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()){
                     continue;// go to next line
                 }
+                String str = "Hello World";
+                int mid = line.length() /2;
 
+                String firstHalf = line.substring(0, mid); // Extracts the first half of the string
+                String secondHalf = line.substring(mid); // Extracts the second half of the string
 
+                for (int i=0; i < secondHalf.length(); i++){
+                    char c = secondHalf.charAt(i);
+                    if (firstHalf.contains(Character.toString(c))){
+                        doubledItems.add(c);
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
