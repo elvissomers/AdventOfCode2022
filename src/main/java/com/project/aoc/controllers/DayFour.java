056be2e341a6524486fd10db099200d09eac269b
elvissomers <elvissomers98@gmail.com>
2023-04-17 18:44:51 +0200

    Update DayFour.java
    finish part 2

--- src/main/java/com/project/aoc/controllers/DayFour.java
+++ src/main/java/com/project/aoc/controllers/DayFour.java
@@ -47,4 +47,32 @@ public class DayFour {
         return count;
     }
 
+    @GetMapping("2")
+    public int getAnswerTwo() {
+        String inputFilePath = "C:\\Users\\elvis\\projects\\aoc_new\\data\\scraped_data_4.txt";
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            String line;
+            reader:
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;// go to next line
+                }
+                String[] numberStrings = line.split("[-,]");
+                int[] numbers = Arrays.stream(numberStrings).mapToInt(Integer::parseInt).toArray();
+
+                if ((numbers[3] >= numbers[1] && numbers[2] <= numbers[1]) || numbers[1] >= numbers[3] && numbers[0] <= numbers[3]) {
+                    count += 1;
+                }
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return count;
+    }
+
 }
