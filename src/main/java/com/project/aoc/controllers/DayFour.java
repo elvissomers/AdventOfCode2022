851fa6d7a2ae1073f784fa613797faf6efadf6b7
elvissomers <elvissomers98@gmail.com>
2023-04-18 12:12:18 +0200

    Update DayFour.java

--- src/main/java/com/project/aoc/controllers/DayFour.java
+++ src/main/java/com/project/aoc/controllers/DayFour.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("4")
