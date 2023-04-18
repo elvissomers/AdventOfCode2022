677886aff315397345ed53f255a82ef2ea96f3fb
elvissomers <elvissomers98@gmail.com>
2023-04-18 14:01:29 +0200

    Update AocApplicationTests.java
    test finished

--- src/test/java/com/project/aoc/AocApplicationTests.java
+++ src/test/java/com/project/aoc/AocApplicationTests.java
@@ -1,14 +1,53 @@
 package com.project.aoc;
 
+import com.project.aoc.controllers.DayFive;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AocApplicationTests {
 
+	DayFive testCase = new DayFive();
 	@Test
-	void contextLoads() {
-		
+	void getStacksSimpleTestOne() {
+		String line1 = "    [D]    \n";
+		String line2 = "[N] [C]    \n";
+		String line3 = "[Z] [M] [P]";
+		List<String> lines = Arrays.asList(line1, line2, line3);
+
+		List<Deque<Character>> stacks = testCase.getStartStacks(lines);
+		assertEquals(stacks.get(0).getLast(), 'Z');
+
+	}
+
+	@Test
+	void getStacksSimpleTestTwo() {
+		String line1 = "    [D]    \n";
+		String line2 = "[N] [C]    \n";
+		String line3 = "[Z] [M] [P]";
+		List<String> lines = Arrays.asList(line1, line2, line3);
+
+		List<Deque<Character>> stacks = testCase.getStartStacks(lines);
+		assertEquals(stacks.get(1).getFirst(), 'D');
+
+	}
+
+	@Test
+	void getStacksSimpleTestThree() {
+		String line1 = "    [D]    \n";
+		String line2 = "[N] [C]    \n";
+		String line3 = "[Z] [M] [P]";
+		List<String> lines = Arrays.asList(line1, line2, line3);
+
+		List<Deque<Character>> stacks = testCase.getStartStacks(lines);
+		assertEquals(stacks.get(1).getLast(), 'M');
+
 	}
 
 }
