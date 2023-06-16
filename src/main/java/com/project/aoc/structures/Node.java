db0ae3513376ab03ce005ac40bca88749942fd71
elvissomers <elvissomers98@gmail.com>
2023-06-16 11:09:56 +0200

    Changed node data to "int"

--- src/main/java/com/project/aoc/structures/Node.java
+++ src/main/java/com/project/aoc/structures/Node.java
@@ -1,21 +1,22 @@
 package com.project.aoc.structures;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Node {
 
-    private final Object data;
+    private final int data;
 
     private final String parent;
 
-    private Set<String> children;
+    private Set<String> children = new HashSet<>();
 
-    public Node(Object data, String parent) {
+    public Node(int data, String parent) {
         this.data = data;
         this.parent = parent;
     }
 
-    public Object getData() {
+    public int getData() {
         return data;
     }
 
