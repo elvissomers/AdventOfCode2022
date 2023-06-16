c461e9566d424ef630fb7739c12176a3192435f0
elvissomers <elvissomers98@gmail.com>
2023-06-16 11:12:45 +0200

    update to add all data up into tree

--- src/main/java/com/project/aoc/controllers/DaySeven.java
+++ src/main/java/com/project/aoc/controllers/DaySeven.java
@@ -23,6 +23,7 @@ public class DaySeven {
         String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_7.txt";
         int ans = 0;
         Tree fileSystem = new Tree();
+        fileSystem.put("/", new Node(0, null)); // Setting base node
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
             String line;
             String currentNodeTag = null;
@@ -33,12 +34,6 @@ public class DaySeven {
                     continue;
                 }
                 String[] words = line.split(" ");
-                if (readingLsOutput) {
-                    int dataSize = (words[0].equals("dir")) ? 0 : Integer.parseInt(words[0]);
-                    Node newNode = new Node(dataSize,currentNodeTag);
-                    fileSystem.put(words[1], newNode);
-                    fileSystem.get(currentNodeTag).addChild(words[1]);
-                }
 
                 if (words[0].equals("$")) { // This is a command
                     if (words[1].equals("cd")) {
@@ -48,12 +43,12 @@ public class DaySeven {
                             assert currentNodeTag != null;
                             currentParentTag = fileSystem.get(currentNodeTag).getParent();
                         } else if (words[2].equals("/")) {
-                            continue;
+                            currentNodeTag = words[2];
                             //TODO : take into account the possibility of multiple "cd /" commands (not the case in input)
                         } else {
                             // TODO: node is already in tree!
                             currentParentTag = currentNodeTag;
-                            currentNodeTag = words[3];
+                            currentNodeTag = words[2];
                         }
 
                     } else if (words[1].equals("ls")) {
@@ -63,6 +58,22 @@ public class DaySeven {
                         readingLsOutput = true;
 
                     }
+                    continue;
+                }
+
+                if (readingLsOutput) {
+                    int dataSize = (words[0].equals("dir")) ? 0 : Integer.parseInt(words[0]);
+                    Node newNode = new Node(dataSize,currentNodeTag);
+                    // TODO: also add data size to all parent node, and to their parent etc
+                    String parentTag = currentParentTag;
+                    while (parentTag != null) {
+                        String currentTag = parentTag;
+                        Node currentNode = fileSystem.get(currentTag);
+                        parentTag = currentNode.getParent();
+                        fileSystem.put(currentTag, new Node(currentNode.getData() + dataSize, parentTag));
+                    }
+                    fileSystem.put(words[1], newNode);
+                    fileSystem.get(currentNodeTag).addChild(words[1]);
                 }
             }
         } catch (IOException e) {
@@ -75,25 +86,7 @@ public class DaySeven {
     /**
      * Helper methods from here
      */
-    public void readListOutput(Tree tree, BufferedReader reader, String nodeName){
-        Node node = tree.get(nodeName);
-        String line;
-        try (reader) {
-        while ((line = reader.readLine()) != null) {
-            if (line.isEmpty()) {
-                continue;// go to next line
-            }
-            String[] words = line.split(" ");
-            if (words[0].equals("$"))
-                return;
+    public void getTotalValueList(Tree fileSystem) {
 
-            int dataSize = (words[0].equals("dir")) ? 0 : Integer.parseInt(words[0]);
-            Node newNode = new Node(dataSize,nodeName);
-            tree.put(words[1], newNode);
-            node.addChild(words[1]);
-        }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
