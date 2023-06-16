48a2bd763fdc2829810d7bd00685630dfec41f84
elvissomers <elvissomers98@gmail.com>
2023-06-16 16:34:41 +0200

    some testing

--- src/main/java/com/project/aoc/AocApplication.java
+++ src/main/java/com/project/aoc/AocApplication.java
@@ -1,5 +1,6 @@
 package com.project.aoc;
 
+import com.project.aoc.controllers.DayEight;
 import com.project.aoc.controllers.DaySeven;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AocApplication {
 
 	public static void main(String[] args) {
-//		DaySeven testSeven = new DaySeven();
-//		testSeven.getAnswerOne();
+//		DayEight test8 = new DayEight();
+//		test8.processInputFile();
+//		test8.setBooleanArray();
+//		test8.checkTreeVisibility(1,4);
 		SpringApplication.run(AocApplication.class, args);
 	}
 
