556b046dd63f867b0757a5b7128600960daca6f2
elvissomers <elvissomers98@gmail.com>
2023-07-13 16:40:16 +0200

    Refactor 2

--- src/main/java/com/project/aoc/controllers/DayTwo.java
+++ src/main/java/com/project/aoc/controllers/DayTwo.java
@@ -12,43 +12,14 @@ import java.util.ArrayList;
 @RequestMapping("2")
 public class DayTwo {
 
+    private final String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_2.txt";
+
     @GetMapping("1")
     public int getAnswerOne(){
-        String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_2.txt";
         int score = 0;
+
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.isEmpty()){
-                    continue;// go to next line
-                }
-                char firstChar = line.charAt(0);
-                char secondChar = line.charAt(2);
-
-                if (secondChar == 'X'){
-                    score += 1;
-                    if (firstChar == 'A'){
-                        score += 3;
-                    } else if (firstChar == 'C'){
-                        score += 6;
-                    }
-                } else if (secondChar == 'Y'){
-                    score += 2;
-                    if (firstChar == 'B'){
-                        score += 3;
-                    } else if (firstChar == 'A'){
-                        score += 6;
-                    }
-                } else if (secondChar == 'Z'){
-                    score += 3;
-                    if (firstChar == 'C'){
-                        score += 3;
-                    } else if (firstChar == 'B'){
-                        score += 6;
-                    }
-                }
-
-            }
+            getScoreFromFileReader(score, reader, true);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -58,50 +29,85 @@ public class DayTwo {
 
     @GetMapping("2")
     public int getAnswerTwo(){
-        String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_2.txt";
         int score = 0;
+
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.isEmpty()){
-                    continue;// go to next line
-                }
-                char firstChar = line.charAt(0);
-                char secondChar = line.charAt(2);
-
-                if (secondChar == 'X'){
-                    if (firstChar == 'A'){
-                        score += 3;
-                    } else if (firstChar == 'B'){
-                        score += 1;
-                    } else if (firstChar == 'C'){
-                        score += 2;
-                    }
-                } else if (secondChar == 'Y'){
-                    score += 3;
-                    if (firstChar == 'A'){
-                        score += 1;
-                    } else if (firstChar == 'B'){
-                        score += 2;
-                    } else if (firstChar == 'C'){
-                        score += 3;
-                    }
-                } else if (secondChar == 'Z'){
-                    score += 6;
-                    if (firstChar == 'A'){
-                        score += 2;
-                    } else if (firstChar == 'B'){
-                        score += 3;
-                    } else if (firstChar == 'C'){
-                        score += 1;
-                    }
-                }
-
-            }
+            getScoreFromFileReader(score, reader, false);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        return score;
+    }
+
+    private void getScoreFromFileReader(int score, BufferedReader reader, boolean strategyOneUsed) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()){
+                continue;// go to next line
+            }
+            char firstChar = line.charAt(0);
+            char secondChar = line.charAt(2);
+
+            score = (strategyOneUsed) ? increaseScoreForStrategyOne(score, firstChar, secondChar) :
+                increaseScoreForStrategyTwo(score, firstChar, secondChar);
+        }
+    }
+
+    private int increaseScoreForStrategyOne(int score, char firstChar, char secondChar) {
+        if (secondChar == 'X'){
+            score += 1;
+            if (firstChar == 'A'){
+                score += 3;
+            } else if (firstChar == 'C'){
+                score += 6;
+            }
+        } else if (secondChar == 'Y'){
+            score += 2;
+            if (firstChar == 'B'){
+                score += 3;
+            } else if (firstChar == 'A'){
+                score += 6;
+            }
+        } else if (secondChar == 'Z'){
+            score += 3;
+            if (firstChar == 'C'){
+                score += 3;
+            } else if (firstChar == 'B'){
+                score += 6;
+            }
+        }
+        return score;
+    }
+
+    private int increaseScoreForStrategyTwo(int score, char firstChar, char secondChar) {
+        if (secondChar == 'X'){
+            if (firstChar == 'A'){
+                score += 3;
+            } else if (firstChar == 'B'){
+                score += 1;
+            } else if (firstChar == 'C'){
+                score += 2;
+            }
+        } else if (secondChar == 'Y'){
+            score += 3;
+            if (firstChar == 'A'){
+                score += 1;
+            } else if (firstChar == 'B'){
+                score += 2;
+            } else if (firstChar == 'C'){
+                score += 3;
+            }
+        } else if (secondChar == 'Z'){
+            score += 6;
+            if (firstChar == 'A'){
+                score += 2;
+            } else if (firstChar == 'B'){
+                score += 3;
+            } else if (firstChar == 'C'){
+                score += 1;
+            }
+        }
         return score;
     }
 }
