e49b8bec60e9a16595ccadd918734a40394858c6
elvissomers <elvissomers98@gmail.com>
2023-07-13 16:27:58 +0200

    Refactor 1.2

--- src/main/java/com/project/aoc/controllers/DayOne.java
+++ src/main/java/com/project/aoc/controllers/DayOne.java
@@ -19,7 +19,6 @@ public class DayOne {
 
     private final String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_1.txt";
 
-
     @GetMapping("1")
     public Integer getAnswerOne(){
         ArrayList<Integer> elfCalories = new ArrayList<>();
@@ -33,8 +32,24 @@ public class DayOne {
         return elfCalories.stream().max(Integer::compareTo).orElse(0);
     }
 
+    @GetMapping("2")
+    public Integer getAnswerTwo(){
+        ArrayList<Integer> elfCalories = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            getElfCaloriesFromFileReader(elfCalories, reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return elfCalories.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
     private void getElfCaloriesFromFileReader(ArrayList<Integer> elfCalories,
-                                                            BufferedReader reader) throws IOException {
+                                              BufferedReader reader) throws IOException {
         String line;
         int elfCount = 0;
         elfCalories.add(0);
@@ -48,35 +63,5 @@ public class DayOne {
                 elfCalories.add(0);
             }
         }
-    }
-
-    @GetMapping("2")
-    public ResponseEntity<Integer> getAnswerTwo(){
-        ArrayList<Integer> elfCalories = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
-            String line;
-            int elfCount = 0;
-            elfCalories.add(0);
-            while ((line = reader.readLine()) != null) {
-                if (!line.trim().isEmpty()) {
-                    Integer currentCalories = elfCalories.get(elfCount);
-                    elfCalories.set(elfCount, currentCalories + Integer.parseInt(line.trim()));
-                } else {
-                    elfCount += 1;
-                    elfCalories.add(0);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        int answer = elfCalories.stream()
-                .sorted(Comparator.reverseOrder())
-                .limit(3)
-                .mapToInt(Integer::intValue)
-                .sum();
-
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(answer);
-
     }
 }
