4b18c72a62fec6e24e8cdb8eb269ff4a1fa67bc1
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-17 15:41:15 +0200

    remove unused imports

--- src/main/java/com/project/aoc/controllers/DayThree.java
+++ src/main/java/com/project/aoc/controllers/DayThree.java
@@ -8,10 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("3")
