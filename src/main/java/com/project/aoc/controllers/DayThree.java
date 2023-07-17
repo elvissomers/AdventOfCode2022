9dd54c1491af20d08943baca1d2b8d5f06848ffd
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-17 15:24:43 +0200

    refactored 3.1

--- src/main/java/com/project/aoc/controllers/DayThree.java
+++ src/main/java/com/project/aoc/controllers/DayThree.java
@@ -1,4 +1,5 @@
 package com.project.aoc.controllers;
+import com.project.aoc.exception.ElementNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,45 +17,64 @@ import java.util.stream.Collectors;
 @RequestMapping("3")
 public class DayThree {
 
+    private final String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_3.txt";
+
     @GetMapping("1")
     public int getAnswerOne(){
-        String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_3.txt";
         List<Character> doubledItems = new ArrayList<>();
+
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
-            String line;
-            reader: while ((line = reader.readLine()) != null) {
-                if (line.isEmpty()){
-                    continue;// go to next line
-                }
-                int mid = line.length() /2;
-
-                String firstHalf = line.substring(0, mid); // Extracts the first half of the string
-                String secondHalf = line.substring(mid); // Extracts the second half of the string
-
-                for (int i=0; i < secondHalf.length(); i++){
-                    char c = secondHalf.charAt(i);
-                    if (firstHalf.contains(Character.toString(c))){
-                        doubledItems.add(c);
-                        continue reader;
-                    }
-                }
-            }
+            getDoubledItemsFromFileReader(doubledItems, reader);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        List<Integer> convertedNumbers = doubledItems.stream()
+        return getSumOfCharacterValues(doubledItems);
+    }
+
+    private void getDoubledItemsFromFileReader(List<Character> doubledItems, BufferedReader reader) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()){
+                continue;
+            }
+            doubledItems.add(findCommonCharacterInFirstAndSecondHalfOfString(line));
+        }
+    }
+
+    private char findCommonCharacterInFirstAndSecondHalfOfString(String string) {
+        int mid = string.length() /2;
+
+        String firstHalf = string.substring(0, mid);
+        String secondHalf = string.substring(mid);
+
+        try {
+            return findCommonCharacterInStrings(firstHalf, secondHalf);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+            return 'E';
+        }
+    }
+
+    private char findCommonCharacterInStrings(String string1, String string2) throws ElementNotFoundException {
+        for (int i=0; i < string1.length(); i++){
+            char c = string1.charAt(i);
+            if (string2.contains(Character.toString(c))){
+                return c;
+            }
+        }
+        throw new ElementNotFoundException("No common character in these strings!");
+    }
+
+    private int getSumOfCharacterValues(List<Character> doubledItems) {
+        return doubledItems.stream()
+                // This map converts the characters to integers using the following rule: a-z = 1-26; A-Z = 27-52
                 .map(c -> (c >= 'a' && c <= 'z') ? (c - 'a' + 1) : (c >= 'A' && c <= 'Z') ? (c - 'A' + 27) : 0)
-                .collect(Collectors.toList());
-
-        int sum = convertedNumbers.stream().mapToInt(Integer::intValue).sum();
-
-        return sum;
+                .mapToInt(Integer::intValue).sum();
     }
 
     @GetMapping("2")
     public int getAnswerTwo() {
-        String inputFilePath = "C:\\Users\\eso13215\\IdeaProjects\\aoc_2022\\data\\scraped_data_3.txt";
         List<Character> doubledItems = new ArrayList<>();
         List<String> group = new ArrayList<>();
         int count = 0;
